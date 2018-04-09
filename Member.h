4d4b6ef5afe98e7fafefd3b26576e7a9b16a6a26
GiladRozmarin <33815204+giladrozmarin@users.noreply.github.com>
2018-04-09 16:25:13 +0300

    Update Member.h

--- Member.h
+++ Member.h
@@ -1,31 +1,28 @@
-/**
- * Member.h
- * 
- * Created on: 02/04/18
- *    Author : nati & gilad 
- * 
- * */
-#include <iostream>
- #include <vector>
- 
- 
- 
 #pragma once
-class Member{
-    private: 
-      std::vector <Member> _listOfFollowing; 
-      std::vector <Member> _listOfFollowers;
-      int _id;
-     static int  _count;
+#include <iostream>
+#include <map>
 
-public:
-Member();
-~Member();
-void follow(Member&);
-void unfollow(Member&);
-unsigned  int numFollowing();
-unsigned int numFollowers();
-static int count(){   //return the number of the members in the Network
-		return _count;
-}
+
+class Member{
+
+
+static int count_members; //the number of the member in the network
+static int nextId; // the id of the next member that sign up
+
+	int id;                     //the id of the member
+        std::map <int, Member *> follows;  //the friends the member following
+	std::map <int, Member *> followers;  //the members that follows the member
+ 	
+	public:
+  	Member();		 //construter
+	~Member();		 //destruter
+	int getId();		 //return the id of the member 
+        int numFollowing();      //return the number of friends the member following
+	int numFollowers();      //return the number of friends that follows the member
+	void follow (Member &);	 //follow after the Member - input 
+	void unfollow (Member &);  //unfollow after the Member - input
+	
+	static int count(){   //return the number of the members in the Network
+		return count_members;
+  	}
 };
