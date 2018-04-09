3a79d2e8fc013e430bb8715e1a8b8a31b7b26d1a
Nati <nati070@walla.com>
2018-04-09 19:02:01 +0300

    fix bug

--- Member.cpp
+++ Member.cpp
@@ -1,58 +1,83 @@
-#include <iostream>
-#include <map>
+
 #include "Member.h"
+#include <map>
+#include <iostream>
 using namespace std;
 
+ 
+    int Member:: otherId = 0; //Initilization
+    int Member::_count = 0; //Initilizationd
 
- int Member::count_members=0; //initilization
- int Member::nextId=0; // initilization
+    Member::Member(){   //constructer 
+      (*this)._id = otherId;
+      _count++;
+      otherId++;
+    }
+    
+ 
+    int Member::getId(){ // implimentation return id;
+        return this->_id;
+    }
+    
+      void Member::follow(Member &newMember){
+        //check if i'm the Member;
+        if(&newMember == this){
+            return;
+        }
+        //check if the Member is inside the Vector already;
+        if(this->_listOfFollowing.count(newMember.getId()) == 1){
+            return;
+        }
+        //add new follow member;
+        this->_listOfFollowing.insert(pair<int , Member *>(newMember.getId() ,&newMember));
+        newMember._listOfFollowers.insert(pair<int ,Member *>(this->_id , this));
+    }
 
-	Member::Member(){		  //implimentation construter
-		count_members++;
-		(*this).id=nextId;
-		nextId++;
-	}
 
-	int Member::getId(){       //implimentation return id
-		return this->id;
-  	}
+    void  Member::unfollow(Member &delMember){    
 
-
-	int Member::numFollowing(){       //implimentation return num_of_Following
-		return this->follows.size();
-  	}
-
-	int Member::numFollowers(){       //implimentation return num_of_Followers
-		return this->followers.size();
-  	}
-
-	void Member::follow (Member &m){       //implimentation update following
-		if(this->follows.count(m.getId())==0 && m.getId()!=this->id){
- 			this->follows.insert(pair<int,Member *>(m.getId(),&m));
-			m.followers.insert(pair<int,Member *>(this->id,this));
-		}
-	}
-
-	void Member::unfollow (Member &m){       //implimentation update followers
-		if(this->follows.count(m.getId())!=0){
-		        this->follows.erase(m.getId());
-			m.followers.erase(this->id);
-		}
-	}
-
-	Member::~Member(){	 //implimentation
-		count_members--;                                     //update the num of members
-		map<int,Member *>::iterator j=followers.begin();
-		while(j!=followers.end()){                           //update my followers list of following
-			if((j->second)->follows.count(this->id)!=0)
-		              (j->second)->follows.erase((j->second)->follows.find(this->id));
+      //check if thr current Member his the newMember;
+        if(&delMember == this){
+        return;
+        }
+         //check if the Member is inside the  and erase him;
+       if(this->_listOfFollowing.count(delMember.getId()) != 1){
+           return;
+       }
+       this->_listOfFollowing.erase(delMember.getId());
+       delMember._listOfFollowers.erase(this->_id);
+        
+    
+     
+     //"the Member not exist"
+    }
+  
+  unsigned int  Member::numFollowers(){
+       return _listOfFollowers.size();
+   }
+   unsigned int  Member::numFollowing(){
+       return _listOfFollowing.size();
+   }  
+Member::~Member(){	 //implimentation
+		                                     //update the num of members
+		map<int,Member *>::iterator j=_listOfFollowers.begin();
+		while(j!=_listOfFollowers.end()){                           //update my followers list of following
+			if((j->second)->_listOfFollowing.count(this->_id)!=0)
+		              (j->second)->_listOfFollowing.erase((j->second)->_listOfFollowing.find(this->_id));
 			j++;
 		}
 
-		map<int,Member *>::iterator i=follows.begin();
-		while(i!=follows.end()){                              //update my following num of followers
-			if((i->second)->followers.count(this->id)!=0)
-		              (i->second)->followers.erase((i->second)->followers.find(this->id));
+		map<int,Member *>::iterator i=_listOfFollowing.begin();
+		while(i!=_listOfFollowing.end()){                              //update my following num of followers
+			if((i->second)->_listOfFollowers.count(this->_id)!=0)
+		              (i->second)->_listOfFollowers.erase((i->second)->_listOfFollowers.find(this->_id));
 			i++;
 		}	
-	}
+        _count--;
+}
+
+
+
+
+
+
